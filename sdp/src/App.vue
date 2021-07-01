fe7a1e6abf6f02c0469b8bab19eac0646d51af8a
RyuK-H <ryugihyeok@gmail.com>
2021-07-01 22:36:17 +0900

    ENH: 기본 환경 구성

---
+++ sdp/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" class="ryuk_bg">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.ryuk_bg {
+  background-color: #2c3e50;
+  background-color: rgba(0, 0, 0);
+  background-image:url(./assets/life.png);
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  overflow: hidden;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-attachment: fixed;
+}
+</style>
---
+++ sdp/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="test">
+    가능~
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.test {
+  color: pink;
+  margin-top: 100px;
+  font-size: 30px;
+  font-weight: bold;
+}
+
+</style>
